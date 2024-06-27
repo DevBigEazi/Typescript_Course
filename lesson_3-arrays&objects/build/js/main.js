8c2e428af3c1321c07d9574c8a0b9cdd51dbfdfd
devbigeazi <adesholatajudeen1@gmail.com>
2024-06-27 10:22:58 +0100

    second commit

--- lesson_3-arrays&objects/build/js/main.js
+++ lesson_3-arrays&objects/build/js/main.js
@@ -4,17 +4,58 @@ let students = ["Wale", "Akeem", 220];
 let mixedData = ["EVM", 2024, true];
 stringArr[0] = "Hi";
 stringArr.push("Hey");
-console.log(stringArr.length);
-console.log(stringArr.length - 1);
 students[0] = 123;
 students.unshift("Tunde");
-console.log(students);
 // // stringArr is assignable to the union types variable and not vice versa only because the union types arr accept string
 // students = stringArr;
 // mixedData = stringArr;
 // mixedData = students;
 let test = [];
-let bands = [];
+let bands = []; // athe variable is annotated here
 bands.push("Van Halen");
-console.log(bands);
-// Turple
+// if you want to be more strict in define something that is locked in to a type in specific element position in a specific link of an array, all you really want to create is tuple.
+// e.g
+let myTuple = ["vanhalen", 200, true];
+let mixed = ["john", 1, false];
+myTuple[1] = 20;
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: "Isiaq",
+    prop2: true,
+};
+exampleObj.prop1 = "Adeshola";
+let evh = {
+    name: "Eddie",
+    active: true,
+    albums: [1001, 1002, "UI1003"],
+};
+let jp = {
+    name: "Jimmy",
+    active: false,
+    albums: [1, 2, "VII"],
+};
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}!`;
+    }
+    return "Hello!";
+    //   return `Hello ${guitarist.name?.toUpperCase()}!`;
+};
+console.log(greetGuitarist(evh));
+// Enum
+/*
+Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime.
+ */
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 0] = "U";
+    Grade[Grade["D"] = 1] = "D";
+    Grade[Grade["C"] = 2] = "C";
+    Grade[Grade["B"] = 3] = "B";
+    Grade[Grade["A"] = 4] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- lesson_3-arrays&objects/src/main.ts
+++ lesson_3-arrays&objects/src/main.ts
@@ -7,13 +7,8 @@ let mixedData = ["EVM", 2024, true];
 stringArr[0] = "Hi";
 stringArr.push("Hey");
 
-console.log(stringArr.length);
-console.log(stringArr.length - 1);
-
 students[0] = 123;
 students.unshift("Tunde");
-
-console.log(students);
 
 // // stringArr is assignable to the union types variable and not vice versa only because the union types arr accept string
 // students = stringArr;
@@ -22,9 +17,75 @@ console.log(students);
 
 let test = [];
 
-let bands: string[] = [];
+let bands: string[] = []; // athe variable is annotated here
 bands.push("Van Halen");
 
-console.log(bands);
+// if you want to be more strict in define something that is locked in to a type in specific element position in a specific link of an array, all you really want to create is tuple.
+// e.g
+let myTuple: [string, number, boolean] = ["vanhalen", 200, true];
 
-// Turple
+let mixed = ["john", 1, false];
+
+myTuple[1] = 20;
+
+let myObj: object;
+
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+
+const exampleObj = {
+  prop1: "Isiaq",
+  prop2: true,
+};
+
+exampleObj.prop1 = "Adeshola";
+
+// interface Guitarist {
+//   name: string;
+//   active?: boolean;
+//   albums: (number | string)[];
+// }
+
+type Guitarist = {
+  name?: string;
+  active?: boolean;
+  albums: (number | string)[];
+};
+
+let evh: Guitarist = {
+  name: "Eddie",
+  active: true,
+  albums: [1001, 1002, "UI1003"],
+};
+
+let jp: Guitarist = {
+  name: "Jimmy",
+  active: false,
+  albums: [1, 2, "VII"],
+};
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name) {
+    return `Hello ${guitarist.name.toUpperCase()}!`;
+  }
+  return "Hello!";
+  //   return `Hello ${guitarist.name?.toUpperCase()}!`;
+};
+console.log(greetGuitarist(evh));
+
+// Enum
+/*
+Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime.
+ */
+
+enum Grade {
+  U,
+  D,
+  C,
+  B,
+  A,
+}
+
+console.log(Grade.U);
